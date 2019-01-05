8572f6cdf144bf2daaca258c134ba9f1b01da51d
manishtiwari24 <39103906+manishtiwari24@users.noreply.github.com>
2019-01-05 14:17:04 -0500

    Final changes

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mtiwari24/multi-client:latest -t mtiwari24/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t mtiwari24/multi-server:latest -t mtiwari24/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t mtiwari24/multi-worker:latest -t mtiwari24/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push mtiwari24/multi-client:latest
+docker push mtiwari24/multi-server:latest
+docker push mtiwari24/multi-worker:latest
+
+docker push mtiwari24/multi-client:$SHA
+docker push mtiwari24/multi-server:$SHA
+docker push mtiwari24/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=mtiwari24/multi-client:$SHA
+kubectl set image deployments/server-deployment server=mtiwari24/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=mtiwari24/multi-worker:$SHA
